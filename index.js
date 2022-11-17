81c5c8384aa6a703a16d3e414beb8baea7d39ac2
Amauri <hamauri@aol.com>
2022-11-17 13:21:32 -0800

    prompt complete

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const questions = require('./lib/questions'); // questions for inquirer
+const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
-const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
+const Manager = require('./lib/Manager');
 
-function init(){
-    
+// list of employees 
+const employeeList = [];
+
+function init() {
+    fs.copyFile('./src/index.html', './dist/index.html', () => { })
+    fs.copyFile('./src/index.css', './dist/index.css', () => { })
+    promptUser();
+}
+
+// prompt for user input
+const promptUser = () => {
+    inquirer
+        .prompt(questions)
+        .then(answers => {
+            //employeeList.push(answers);
+            console.log('');
+            addEmployee(answers);
+            if (answers.askAgain){
+                promptUser();
+            }
+            else
+                buildWebsite();
+        })
+}
+
+const addEmployee = (employee) => {
+    const { employeeType, name, id, email } = employee
+
+    switch (employeeList) {
+        case 'Intern':
+            const { school } = employee
+            break;
+        case 'Engineer':
+            const { github } = employee
+            break;
+        case 'Manager':
+            const { officeNumber } = employee
+            break;
+    }
+}
+
+//builds the website
+const buildWebsite = () => {
+
 }
 
 init();
